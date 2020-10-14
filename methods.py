023de35987ccdd30054275b4131e57a34aad425f
jbrentfoster <jbrentfoster@yahoo.com>
2020-10-14 13:20:36 -0400

    Changed websocket logging

--- methods.py
+++ methods.py
@@ -34,8 +34,8 @@ def run_command(ajax_handler, cmd, cmd_arg):
             i += 1
             logging.info(line.decode('utf-8'))
             stdout_text += line.decode('utf-8')
-            if i%10 == 0:
-                ajax_handler.send_message_open_ws("script progressing" + "."*i)
+            if i%50 == 0:
+                ajax_handler.send_message_open_ws("script progressing...")
         result = {'action': 'collect', 'status': 'completed', 'body': 'Successfully executed command on the server.'}
         return result
     except Exception as err:
